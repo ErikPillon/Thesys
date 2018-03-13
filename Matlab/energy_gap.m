c64d0ebfae319a4a357911f9cf16d9c69c71c484
Erik Pillon <erik.pillon@gmail.com>
2018-03-13 11:43:49 +0100

    First saving of the main file for the energy gap script

---
+++ Matlab/energy_gap.m
@@ -0,0 +1,27 @@
+% algorithm for the implementation of the ewald summation
+% following notes on pages 3 and following
+% 
+% suggested reference "Ewald-Rastelli"
+
+%% initialize all the variables
+alpha = 1;
+vol = 2;
+gam = gamma(alpha/2);
+c = 5; % convergence coefficient
+
+delta = [1/2, 1/2]; % displacement of the electron
+
+G_cut = 10;
+R_cut = 10;
+
+
+
+
+
+
+
+
+
+
+
+
